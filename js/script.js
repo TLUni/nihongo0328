0bf3f8c270effc0ee18daed726b1344f678f92da
TLUni <77527500+TLUni@users.noreply.github.com>
2021-03-28 12:29:38 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	$("#p1").click(function(){
+        $("#p1").attr("src","images/p1_2.png");
+    });
+
+    $("#p2").click(function(){
+        $("#p2").attr("src","images/p2_2.png");
+    });
+
+    $("#p3").click(function(){
+        $("#p3").attr("src","images/p3_2.png");
+    });
+
+    $("#p4").click(function(){
+        $("#p4").attr("src","images/p4_2.png");
+    });
+
+    $("#p5").click(function(){
+        $("#p5").attr("src","images/p5_2.png");
+    });
+
+    $("#p6").click(function(){
+        $("#p6").attr("src","images/p6_2.png");
+    });
+  
+});
